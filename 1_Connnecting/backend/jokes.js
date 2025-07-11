5e22321255072de510dc514b6dce13ece13a8553
Mrigyisha <mrigyishasawant@gmail.com>
2025-07-11 19:10:28 +0530

    first project - connecting

---
+++ 1_Connnecting/backend/jokes.js
@@ -0,0 +1,29 @@
+const jokes =[
+    {
+        id:1,
+        title:"A joke",
+        content:"This is a joke"
+    },
+    {
+        id:2,
+        title:"A second joke",
+        content:"This is a second joke"
+    },
+    {
+        id:3,
+        title:"A third joke",
+        content:"This is a third joke"
+    },
+    {
+        id:4,
+        title:"A fourth joke",
+        content:"This is a fourth joke"
+    },
+    {
+        id:5,
+        title:"A fifth joke",
+        content:"This is a fifth joke"
+    }
+]
+
+export default jokes
